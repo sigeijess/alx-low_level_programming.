0f8109fa7e73d9cfe9841d70be708623e0e5193c
sigeijess <sigeijesse@gmail.com>
2023-02-28 11:55:33 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,11 @@
  * print_alphabet - print all alphabet in lowercase
  */
 
-void print_alphabet (void)
+void print_alphabet(void)
 {
 	char letter;
 
-	for(letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 
 	_putchar('\n');
