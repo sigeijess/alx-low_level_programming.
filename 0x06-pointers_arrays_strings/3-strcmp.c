855373db57d1877eeef8b8adb20ffb9e27fad16a
sigeijess <sigeijesse@gmail.com>
2023-03-02 12:55:28 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,37 +1,24 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account
- * @month: month in number format
- * @day: day of month
- * @year: year
- * Return: void
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
+ *
+ * Return: s1[i] - s2[i]
  */
-
-void print_remaining_days(int month, int day, int year)
+int _strcmp(char *s1, char *s2)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (month > 2 && day >= 60)
+		if (s1[i] != s2[i])
 		{
-			day++;
+			return (s1[i] - s2[i]);
 		}
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
+		i++;
 	}
-	else
-	{
-		if (month == 2 && day == 60)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
-	}
+	return (0);
 }
 
