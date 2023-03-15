d1af3074837945babfe43fa047eca8bea4b06f65
sigeijess <sigeijesse@gmail.com>
2023-03-15 12:53:05 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,43 +1,22 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * argstostr - main entry
- * @ac: int input
- * @av: double pointer array
- * Return: 0
+ * free_grid - frees 2d array
+ * @grid: 2d grid
+ * @height: height dimension of grid
+ * Description: frees memory of grid
+ * Return: nothing
+ *
  */
-
-char *argstostr(int ac, char **av)
+void free_grid(int **grid, int height)
 {
-	int i, n, r = 0, l = 0;
-	char *str;
+	int i;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	for (i = 0; i < ac; i++)
+	for (i = 0; i < height; i++)
 	{
-		for (n = 0; av[i][n]; n++)
-			l++;
+		free(grid[i]);
 	}
-	l += ac;
-
-	str = malloc(sizeof(char) * l + 1);
-	if (str == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-	for (n = 0; av[i][n]; n++)
-	{
-		str[r] = av[i][n];
-		r++;
-	}
-	if (str[r] == '\0')
-	{
-		str[r++] = '\n';
-	}
-	}
-	return (str);
+	free(grid);
 }
 
